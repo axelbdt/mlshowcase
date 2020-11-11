0f8572fa56f0b32bdb7155d4c931775ae7ae8615
Axel Baudot <axel.baudot@ecoco2.com>
2020-11-11 15:46:39 +0100

    add randomness to data generation

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,34 @@
 
 #[macro_use] extern crate rocket;
 
+
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
+
 #[derive(Serialize)]
 struct Point {
-    x: i32,
-    y: i32
+    x: f32,
+    y: f32
 }
 
 #[get("/data")]
+// fn data() -> Json<Vec<Point>> {
 fn data() -> Json<Vec<Point>> {
-    let my_vec = vec![
-            Point {x:-10, y:10},
-            Point {x:0, y:10},
-            Point {x:10, y:5},
-        ];
+    let mut rng = thread_rng();
+    let normal_x = Normal::new(2.0, 3.0).unwrap();
+    let normal_y = Normal::new(5.0, 1.0).unwrap();
+    let v: Vec<Point> = (0..100).map(|_| {
+        let x = normal_x.sample(&mut rng);
+        let y = normal_y.sample(&mut rng);
+        Point {x, y}
+    }).collect();
+    // let v = normal.sample(&mut rng);
 
-    Json(my_vec)
+    Json(v)
 }
 
 fn main() {
