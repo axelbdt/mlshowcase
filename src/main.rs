ac2ef729a6f12b90c8ace849e5d3e7057340218a
Axel Baudot <axel.baudot@ecoco2.com>
2020-11-12 15:12:05 +0100

    extended data generation function to generate correlated data

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
-use rand::thread_rng;
-use rand_distr::{Distribution, Normal};
+use rand::prelude::*;
+use rand_distr::{Distribution, StandardNormal};
+use rand::rngs::StdRng;
 
 #[derive(Serialize)]
 struct Point {
@@ -17,17 +18,26 @@ struct Point {
 }
 
 #[get("/data")]
-// fn data() -> Json<Vec<Point>> {
 fn data() -> Json<Vec<Point>> {
-    let mut rng = thread_rng();
-    let normal_x = Normal::new(2.0, 3.0).unwrap();
-    let normal_y = Normal::new(5.0, 1.0).unwrap();
+    let mut rng = StdRng::seed_from_u64(42);
+
+    let mu: [f32; 2] = [6.0, 5.0];
+    let sigma: [[f32; 2]; 2] = [[2.0, 1.0], [1.0, 3.0]];
+    
+    // Cholesky decomposition
+    let a = sigma[0][0].sqrt();
+    let b = sigma[0][1] / a;
+    let c = (sigma[1][1]).sqrt() - b * b;
+
+    let sqrt_sigma = [ [a, 0.], [b, c]];
+
     let v: Vec<Point> = (0..100).map(|_| {
-        let x = normal_x.sample(&mut rng);
-        let y = normal_y.sample(&mut rng);
+        let x0: f32 = rng.sample(StandardNormal);
+        let y0: f32 = rng.sample(StandardNormal);
+        let x = x0 * sqrt_sigma[0][0] + mu[0];
+        let y = x0 * sqrt_sigma[1][0] + y0 * sqrt_sigma[1][1] + mu[1];
         Point {x, y}
     }).collect();
-    // let v = normal.sample(&mut rng);
 
     Json(v)
 }
