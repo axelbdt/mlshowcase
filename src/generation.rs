eabd2849598e32a39eaa6adf7ebaaaafe0937be2
Axel Baudot <axel.baudot@ecoco2.com>
2020-11-14 16:41:37 +0100

    refactor to make generation return Vec<Vector2> instead of matrix

--- src/generation.rs
+++ src/generation.rs
@@ -1,7 +1,5 @@
 extern crate nalgebra as na;
 
-use crate::routes::Point;
-
 use na::{Dynamic, Matrix, Matrix2, U2, Vector2, VecStorage};
 use rand::prelude::*;
 use rand_distr::StandardNormal;
@@ -10,15 +8,13 @@ use rand_distr::StandardNormal;
 // two rows using 32-bit floats.
 type Matrix2xXf32 = Matrix<f32, U2, Dynamic, VecStorage<f32, U2, Dynamic>>;
 
-pub fn generate_dataset(seed: u64) -> Vec<Point> {
+pub fn generate_dataset(seed: u64) -> Vec<Vector2<f32>> {
     let mut rng = StdRng::seed_from_u64(seed);
 
     let sigma: Matrix2<f32> = Matrix2::new(2.0, 1.0 ,1.0, 3.0);
     let mu: Vector2<f32> = Vector2::new(6.0, 5.0);
     
-    let dataset = generate_bivariate_normal_sample(mu, sigma, 100, &mut rng);
-
-    dataset.column_iter().map( |c| Point {x: c[0], y: c[1]}).collect()
+    generate_bivariate_normal_sample(mu, sigma, 100, &mut rng)
 }
 
 pub fn generate_bivariate_normal_sample(
@@ -26,12 +22,11 @@ pub fn generate_bivariate_normal_sample(
     sigma: Matrix2<f32>,
     nb_points: u64,
     rng: &mut StdRng
-    ) -> Matrix2xXf32 {
+    ) -> Vec<Vector2<f32>> {
     let sqrt_sigma = sigma.cholesky().unwrap().l();
 
     let sample = Matrix2xXf32::from_distribution(nb_points as usize, &StandardNormal, rng);
     let data_zero_mean = sqrt_sigma * sample;
 
-    let data: Vec<Vector2<f32>> = data_zero_mean.column_iter().map(|c| c + mu).collect();
-    Matrix2xXf32::from_columns(&data)
+    data_zero_mean.column_iter().map(|c| c + mu).collect()
 }
--- src/routes.rs
+++ src/routes.rs
@@ -6,12 +6,15 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Point {
-    pub x: f32,
-    pub y: f32
+    x: f32,
+    y: f32
 }
 
 #[get("/data")]
 pub fn data() -> Json<Vec<Point>> {
-    let v = generation::generate_dataset(42);
+    let v = generation::generate_dataset(42)
+        .iter()
+        .map( |c| Point {x: c[0], y: c[1]})
+        .collect();
     Json(v)
 }
