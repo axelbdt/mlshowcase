b8d7e845822971351b19aa598593fa5bc3c04ce0
Axel Baudot <axel.baudot@ecoco2.com>
2020-11-14 16:08:50 +0100

    change data generation to use matrix multiplication

--- src/generation.rs
+++ src/generation.rs
@@ -1,17 +1,24 @@
 extern crate nalgebra as na;
 
-use na::base::{Matrix2, Vector2};
+use crate::routes::Point;
+
+use na::{Dynamic, Matrix, Matrix2, U2, Vector2, VecStorage};
 use rand::prelude::*;
-use rand_distr::{Distribution, StandardNormal};
+use rand_distr::StandardNormal;
 
+// Half-dynamically sized and dynamically allocated matrix with
+// two rows using 32-bit floats.
+type Matrix2xXf32 = Matrix<f32, U2, Dynamic, VecStorage<f32, U2, Dynamic>>;
 
-pub fn generate_dataset(seed: u64) -> Vec<[f32;2]> {
+pub fn generate_dataset(seed: u64) -> Vec<Point> {
     let mut rng = StdRng::seed_from_u64(seed);
 
     let sigma: Matrix2<f32> = Matrix2::new(2.0, 1.0 ,1.0, 3.0);
     let mu: Vector2<f32> = Vector2::new(6.0, 5.0);
     
-    generate_bivariate_normal_sample(mu, sigma, 100, &mut rng)
+    let dataset = generate_bivariate_normal_sample(mu, sigma, 100, &mut rng);
+
+    dataset.column_iter().map( |c| Point {x: c[0], y: c[1]}).collect()
 }
 
 pub fn generate_bivariate_normal_sample(
@@ -19,14 +26,12 @@ pub fn generate_bivariate_normal_sample(
     sigma: Matrix2<f32>,
     nb_points: u64,
     rng: &mut StdRng
-    ) -> Vec<[f32;2]> {
-    let sqrt_sigma = sigma.cholesky().unwrap().unpack();
+    ) -> Matrix2xXf32 {
+    let sqrt_sigma = sigma.cholesky().unwrap().l();
 
-    (0..nb_points).map(|_| {
-        let x0: f32 = rng.sample(StandardNormal);
-        let y0: f32 = rng.sample(StandardNormal);
-        let v0 = Vector2::new(x0, y0);
-        let v = sqrt_sigma * v0 + mu;
-        [v[0], v[1]]
-    }).collect()
+    let sample = Matrix2xXf32::from_distribution(nb_points as usize, &StandardNormal, rng);
+    let data_zero_mean = sqrt_sigma * sample;
+
+    let data: Vec<Vector2<f32>> = data_zero_mean.column_iter().map(|c| c + mu).collect();
+    Matrix2xXf32::from_columns(&data)
 }
--- src/routes.rs
+++ src/routes.rs
@@ -6,24 +6,12 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Point {
-    x: f32,
-    y: f32
-}
-
-impl Point {
-    fn from_array(p: &[f32;2]) -> Point {
-        Point {
-            x: p[0],
-            y: p[1]
-        }
-    }
+    pub x: f32,
+    pub y: f32
 }
 
 #[get("/data")]
 pub fn data() -> Json<Vec<Point>> {
-
-    let v = generation::generate_dataset(42)
-        .iter().map(Point::from_array).collect();
-
+    let v = generation::generate_dataset(42);
     Json(v)
 }
